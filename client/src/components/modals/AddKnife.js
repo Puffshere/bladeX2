a9edf579d952c7db5fba770ae644f566f2d44406
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-07-02 20:57:29 -0700

    test route

---
+++ client/src/components/modals/AddKnife.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function AddKnifeModal(){
+
+    const[state, setState] = useState({
+        knife: ''
+    })
+
+
+        return(
+            <div>
+                this is the modal
+            </div>
+        )
+
+}
+
+export default AddKnifeModal;
--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static( 'client/build' ));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
 app.get('/butthole', (req, res) => {
     res.send('fuck you')
+})
+
+app.post('/addknife', (req, res) => {
+    const knifedata = req.body;
 })
 
 app.listen(PORT, () => {
