c2df42fec662be07291677de05d2b88b157636fb
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-07-02 21:00:20 -0700

    route changes

--- server.js
+++ server.js
@@ -7,17 +7,13 @@ const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static( 'client/build' ));
 
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
-app.get('/butthole', (req, res) => {
-    res.send('fuck you')
-})
-
-app.post('/addknife', (req, res) => {
-    const knifedata = req.body;
+app.post('/test', (req, res) => {
+    res.send('testy time')
 })
 
 app.listen(PORT, () => {
