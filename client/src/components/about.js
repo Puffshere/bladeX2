9dbba475bb43f7ca2f2a3828e1b293121904d7de
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-07-15 03:28:21 -0700

    about, contact styling

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 
 const About = () => (
     <Fragment>
-      <h1>About</h1>
-      <h5>This is an app to help you track your knife collection.  Manage your inventory.  Track costs and sell your blades.</h5>
+      <div className='aboutPic'></div>
+      <h1 className='aboutHeader'>About</h1>
+      <h5 className='aboutBody'>This is an app to help you track your knife collection.  Manage your inventory.  Track costs and sell your blades.</h5>
       <br></br>
       <br></br>
       <br></br>
+      <div className='linkStyling'>
     <ul>
     <li className='homeStyling'><Link to="/dashboard">Home</Link></li>
     <li className='contactStyling'><Link to="/contact">Contact</Link></li>
     </ul>
+    </div>
     </Fragment>
   );
 
--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom';
 
 const Contact = () => (
     <Fragment>
-      <h1>Contact</h1>
-      <h5>Contact me at:  shawncurtistaylor@hotmail.com</h5>
+      <div className='aboutPic'></div>
+      <h1 className='aboutHeader'>Contact</h1>
+      <h5 className='aboutBody'>Contact me at:  shawncurtistaylor@hotmail.com</h5>
       <br></br>
       <br></br>
       <br></br>
+      <div className='linkStyling'>
     <ul>
     <li className='homeStyling'><Link to="/dashboard">Home</Link></li>
-    <li className='contactStyling'><Link to="/about">About</Link></li>
+    <li className='aboutStyling'><Link to="/contact">About</Link></li>
     </ul>
+    </div>
     </Fragment>
   );
 
