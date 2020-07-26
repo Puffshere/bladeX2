3e8fc7925597e7a146ea06787f2343aec4df4863
Shawn Taylor <shawncurtistaylor@gmail.com>
2020-07-26 00:00:25 -0700

    code cleanup

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
-
 const app = express();
 dotenv.config();
 const uri = process.env.MONGODB_URI;
